bffd17a9bca54d8e5165fb560ea33f40ad4af943
Chris McIntosh <chocrates@github.com>
2019-12-26 16:28:53 -0600

    Fixing the mockFunctions references

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b1e36a9b58b370f9b5d2c12c330c688a9bb82447fc21f1df78da246e567a4593
-size 2537
+oid sha256:b29d46725adc1d9bc08da10c256cf6adea093d726cbac1e4de32c51ba0607cef
+size 2567
