6273cf233f2a0cf2547be5521b771ac2e11903d0
Chris McIntosh <chocrates@github.com>
2020-01-14 11:39:23 -0600

    Adding closeIssue mock

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f260e248a3dae021326bbf7cb124407b93473df54e3bb9e9899c84706a8aabd
-size 3115
+oid sha256:fc915a81ae498b921ba54af0a16fab27c29d79869a67e6fca468cad5315b0d79
+size 3212
