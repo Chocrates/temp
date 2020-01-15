1860f846d4c4e08824c79bc6bb81e38fcaafee06
Chris McIntosh <chocrates@github.com>
2020-01-15 13:26:04 -0600

    Adding  method

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f3372bcb00acf5dbe1a540c4895a49eccfe82b02c7601643b1e1901cc7bd93
-size 3200
+oid sha256:db484a81402f0ae19ef78532dc0c633e7312bd76c2e7313bf9597475b7865c84
+size 3300
