aea32191a63b7f504f057f28080b50b6a87876b2
Chris McIntosh <chocrates@github.com>
2020-01-16 16:11:45 -0600

    Adding fixture loader

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1245d0fc7f592a8d34c586e05a7b72944880c763fe1640e70ee48528769c5f82
-size 3394
+oid sha256:21db907bdb59de14fc88dbffbb729d696ab4b0f6834fe9459b77e87c3127436f
+size 3522
