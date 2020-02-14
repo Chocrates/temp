ac224c260a6d04aa7ca8a6b1f44222d8329131c4
Chris McIntosh <chocrates@github.com>
2020-02-14 14:53:51 -0600

    updating reset mock

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ead6625eeb93353234dabad5b29caee6bcb4b763135226e5bfc1d6f686d64b43
-size 30332
+oid sha256:0f01c9e87ed199536209b9bc1f51ef72deb80a53a21727fe835f0d5b2802e775
+size 30398
