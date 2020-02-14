0e46e452a1b70a2b1afd67591e91cb745ca689a3
Chris McIntosh <chocrates@github.com>
2020-02-14 13:33:30 -0600

    Refactoring a bit and adding fixtures

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:17cc43a4a7d9688e6c12c4f72b6fdb8fac018cb9bf9a413f82723f401a455746
+size 323
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:21db907bdb59de14fc88dbffbb729d696ab4b0f6834fe9459b77e87c3127436f
-size 3522
+oid sha256:ead6625eeb93353234dabad5b29caee6bcb4b763135226e5bfc1d6f686d64b43
+size 30332
