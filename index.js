9bf3dcbdbef05bbb9e4e32646a6534bcf9dc03b3
Chris McIntosh <chocrates@github.com>
2020-02-16 23:47:28 -0600

    Reworking fixtures so existing tests work

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a84c1e109ed1fc697acd2025904dd7925ab870a005ee2c91d7d36a09ae0999b1
-size 30706
+oid sha256:ca97f707cf05199b561d141c8c6a56b2c72d0b40ffdf994d055261ec9e7cdf50
+size 30714
